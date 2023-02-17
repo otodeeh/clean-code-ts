e434da69a71093389d716b11372239218af98a65
Andre Henrique <andre.lemos@praticabr.com>
2023-02-17 16:30:55 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/singup.spec.ts
+++ src/presentation/controllers/signup/singup.spec.ts
@@ -1,8 +1,11 @@
 import { SignUpController } from './signup'
-import { MissingParamError, InvalidParamError, ServerError } from './erros'
-import { EmailValidator } from '../protocols'
-import { AddAccount, AddAccountModel } from '@/domain/usecases/add-account'
-import { AccountModel } from '@/domain/usecases/models/account'
+import { MissingParamError, InvalidParamError, ServerError } from '../../erros'
+import {
+  EmailValidator,
+  AddAccount,
+  AddAccountModel,
+  AccountModel
+} from './signup-protocols'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { ServerError } from '../controllers/erros/server-error'
+import { ServerError } from '../erros/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
