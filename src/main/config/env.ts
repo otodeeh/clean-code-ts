e6046c230de894b8702a38b21ac3c75ef5fd0361
Andre Henrique <andre.lemos@praticabr.com>
2023-02-27 17:14:14 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export default {
+  mongoUrl:
+    process.env.MONGO_URL ||
+    'mongodb+srv://dehhgl:KPvoQCXs5fGOiBZy@clean-code-api.488qtrx.mongodb.net/test',
+  port: process.env.PORT || 5050
+}
