74bf10ccaded243edcaccff2b3b1f151f0d49eb0
Andre Henrique <andre.lemos@praticabr.com>
2023-02-28 15:35:02 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/logs.spec.ts
+++ src/main/decorators/logs.spec.ts
@@ -37,7 +37,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LogController Decorator', () => {
-  test('should call controller hndle', async () => {
+  test('should call controller handle', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
     const httpRequest = {
@@ -50,5 +50,24 @@ describe('LogController Decorator', () => {
     }
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        paswordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Andre'
+      }
+    })
   })
 })
