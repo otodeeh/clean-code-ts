bc72f761d297d0f8871652e3554bc50152d94fd3
Andre Henrique <andre.lemos@praticabr.com>
2023-02-28 09:42:22 -0300

    chore: jest test:ci config files protocols

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/**-protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
   }
+  // moduleNameMapper: {
+  //   '@/(.*)': '<rootDir>/src/$1'
+  // }
 }
