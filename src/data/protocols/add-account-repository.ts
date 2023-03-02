8eb9a154ed2f59e948d740016fce1ffde63b12e6
Andre Henrique <andre.lemos@praticabr.com>
2023-03-02 15:26:33 -0300

    refactor: move archive to domain

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -1,5 +1,5 @@
 import { AddAccountModel } from '@/domain/usecases/add-account'
-import { AccountModel } from '@/domain/usecases/models/account'
+import { AccountModel } from '@/domain/models/account'
 
 export interface AddAccountRepository {
   add: (accountData: AddAccountModel) => Promise<AccountModel>
