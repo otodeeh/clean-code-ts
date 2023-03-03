b64a3661300db5a957bba45028ad275219a64e65
Andre Henrique <andre.lemos@praticabr.com>
2023-03-03 17:42:43 -0300

    refactor: group validation imports in a single file

--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -1,9 +1,11 @@
 import { Validation } from '../../../presentation/protocols/validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+  CompareFieldsValidation,
+  EmailValidation
+} from '../../../presentation/helpers/validators'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
