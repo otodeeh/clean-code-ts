79ecc5b611a26ff11be51699ff189694bd3667bd
Andre Henrique <andre.lemos@praticabr.com>
2023-03-06 13:30:31 -0300

    refactor: move add-account to a folder

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
 export * from '@/domain/models/account'
-export * from '@/domain/usecases/add-account'
+export * from '@/domain/usecases/add-account/add-account'
 export * from '@/presentation/protocols/validation'
