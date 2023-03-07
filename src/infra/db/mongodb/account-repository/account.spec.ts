b6b380d003c7c3ba6e7f2a33269237a5d894aaa9
Andre Henrique <andre.lemos@praticabr.com>
2023-03-07 18:01:04 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,18 @@ describe('AccountMongoRepository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const fakeAccount = await accountCollection.findOne({ _id: res.insertedId })
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id.toString(), 'any_token')
+    const account = await accountCollection.findOne({ _id: res.insertedId })
+    expect(account.accessToken).toBeTruthy()
+  })
 })
