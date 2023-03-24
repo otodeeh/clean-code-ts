41cd87aa6a72396e0a46c47f7a1d3d0aab8f6b4c
Andre Henrique <andre.lemos@praticabr.com>
2023-03-24 11:02:01 -0300

    feat: ensure SurveyMongoRepository load all surveys on success and load empty list

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,21 @@
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveysRepository
+{
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     const result = await surveyCollection.insertOne(surveyData)
     await surveyCollection.findOne({ _id: result.insertedId })
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return MongoHelper.mapCollection(surveys)
   }
 }
