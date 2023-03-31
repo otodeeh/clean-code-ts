2707dd0a126fbefd1e41c456a89328fe0143848a
Andre Henrique <deh.hgl@gmail.com>
2023-03-31 11:57:14 -0300

    feat: path alias test

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,15 @@ module.exports = {
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    'tests/(.*)': '<rootDir>/tests/$1',
+    'src/(.*)': '<rootDir>/src/$1',
+    'data/(.*)': '<rootDir>/src/data/$1',
+    'domain/(.*)': '<rootDir>/src/domain/$1',
+    'infra/(.*)': '<rootDir>/src/infra/$1',
+    'main/(.*)': '<rootDir>/src/main/$1',
+    'presentation/(.*)': '<rootDir>/src/presentation/$1',
+    'validation/(.*)': '<rootDir>/src/validation/$1'
   }
 }
