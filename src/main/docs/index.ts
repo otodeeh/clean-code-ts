60e5be0654be289d32ef33bf5a4bf355d9a3d9bd
Andre Henrique <deh.hgl@gmail.com>
2023-03-31 12:19:11 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
 import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -9,6 +9,10 @@ export default {
     description:
       'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
   servers: [
     {
@@ -25,6 +29,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
